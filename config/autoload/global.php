966b6a42a820d4db07868dc14d1c043ac232e919
mesija <mesijart@gmail.com>
2016-08-06 18:55:29 +0300

    v 0.1 -- create "Account" module and update zend

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -11,6 +11,18 @@
  * file.
  */
 
-return [
-    // ...
-];
+return array(
+  'db' => array(
+    'driver'         => 'Pdo_Mysql',
+    'dsn'            => 'mysql:host=localhost;dbname=account',
+    'driver_options' => array(
+      PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
+    ),
+  ),
+  'service_manager' => array(
+    'factories' => array(
+      'Zend\Db\Adapter\Adapter'
+        => 'Zend\Db\Adapter\AdapterServiceFactory',
+    ),
+  ),
+);
